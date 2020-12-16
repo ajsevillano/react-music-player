1c7479e34f63724fa3b34022ccede363db90bed2
Sevi <correosevi@gmail.com>
2020-12-16 15:59:40 +0000

    Add basic components styles
    All the styles are now in the style folder, every component has been
    named with _ in front for pure convention. I used in the past folders
    for the components and their styles but I'm going to use this convention
    on this project.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import Song from './components/Song';
 import Player from './components/Player';
 
+//Styles
+import './styles/app.scss';
+
 function App() {
   return (
     <div className="App">
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,7 +1,9 @@
 const Song = () => {
   return (
     <div className="song-container">
+      <h1>Picture</h1>
       <h1>Song</h1>
+      <h1>Artist</h1>
     </div>
   );
 };
