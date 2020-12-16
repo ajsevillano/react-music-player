4fcf4a3d0d13030a306249f587a3bccc5ddeade6
Sevi <correosevi@gmail.com>
2020-12-16 14:53:56 +0000

    Add fontAwesome component and use icons on Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,3 +1,12 @@
+//Import FontAwesome component
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//Import FontAwesome actual icons
+import {
+  faAngleLeft,
+  faAngleRight,
+  faPlay,
+} from '@fortawesome/free-solid-svg-icons';
+
 const Player = () => {
   return (
     <div className="player">
@@ -6,7 +15,15 @@ const Player = () => {
         <input type="range" />
         <p>End time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon
+          className="skip-forward"
+          icon={faAngleRight}
+          size="2x"
+        />
+      </div>
     </div>
   );
 };
