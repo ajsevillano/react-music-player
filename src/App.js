19b1e1a8c53e9232ad751c4160e41fec6702d906
Sevi <correosevi@gmail.com>
2021-01-18 20:33:43 +0000

    Add a function to calculate the played percentage of the song

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
 
@@ -29,10 +30,17 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
+    //Calculate percentage
+
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    console.log(animation);
     setSongInfo({
       ...songInfo,
       currentTime: current,
       duration: duration,
+      animationPercentage: animation,
     });
   };
 
