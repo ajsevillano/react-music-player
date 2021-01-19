5beccf92b2fe655fdf24a862d283d83021091f79
Sevi <correosevi@gmail.com>
2021-01-19 20:30:51 +0000

    Change and add a svg logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,10 @@ import { faMusic } from '@fortawesome/free-solid-svg-icons';
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav>
-      <h1>React Music Player</h1>
+      <div className="logo">
+        <img src={process.env.PUBLIC_URL + '/logo.svg '} alt="Logo" />
+        <h1>React Music Player</h1>
+      </div>
       <button
         onClick={() => {
           setLibraryStatus(!libraryStatus);
